bd1a0354c46800e02e85008e4bc90a9a67d4de44
saurav-prathamint <saurav.prathamint@gmail.com>
2026-06-02 16:49:53 +0530

    Done

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import * as Sentry from "@sentry/react";
-import { BrowserTracing } from "@sentry/react";
+import { browserTracingIntegration } from "@sentry/react";
 
 
 createRoot(document.getElementById("root")!).render(<App />);
